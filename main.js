c9bf141781423a2296ad81c428ade3b36571dbda
Javix33 <javix33@hotmail.com>
2023-07-19 19:10:34 -0500

    nevo sitio falta contacto

--- main.js
+++ main.js
@@ -1,48 +1,32 @@
-//loader//
-window.addEventListener("load", function() {
-    var loaderContainer = document.querySelector(".loader-container");
-    loaderContainer.style.display = "none";
-});
+const slider = document.querySelector(".slider");
+const prevBtn = document.querySelector(".prev-btn");
+const nextBtn = document.querySelector(".next-btn");
 
-var loaderBar = document.querySelectorAll(".loader-bar");
-var loaderPercentage = document.querySelector(".loader-percentage");
-var i = 0;
+let currentIndex = 0;
 
-function loaderAnimation() {
-    if (i == 100) {
-        clearInterval(interval);
-    } else {
-        i++;
-        loaderBar.forEach(function(bar) {
-            bar.style.transform = "scaleY(" + (0.4 + i / 250) + ")";
-        });
-        loaderPercentage.innerHTML = i + "%";
-    }
+function showSlide(index) {
+    const slides = document.querySelectorAll(".slider img");
+    slides.forEach((slide, i) => {
+        if (i === index) {
+            slide.style.display = "block";
+        } else {
+            slide.style.display = "none";
+        }
+    });
 }
 
-var interval = setInterval(loaderAnimation, 20);
-//menu hamburguesa//
-const hamburgerMenu = document.querySelector(".hamburger-menu");
-const menu = document.querySelector("#menu");
-
-hamburgerMenu.addEventListener("click", () => {
-    menu.classList.toggle("active");
-    hamburgerMenu.classList.toggle("active");
-
-    if (menu.classList.contains("active")) {
-        menu.style.maxHeight = menu.scrollHeight + "px";
-    } else {
-        menu.style.maxHeight = null;
-    }
-});
-
-//slider//
-let slides = document.querySelectorAll('.slide');
-let currentSlide = 0;
-let slideInterval = setInterval(nextSlide, 5000);
+function prevSlide() {
+    currentIndex = (currentIndex - 1 + 3) % 3;
+    showSlide(currentIndex);
+}
 
 function nextSlide() {
-    slides[currentSlide].classList.remove('active-slide');
-    currentSlide = (currentSlide + 1) % slides.length;
-    slides[currentSlide].classList.add('active-slide');
+    currentIndex = (currentIndex + 1) % 3;
+    showSlide(currentIndex);
 }
+
+prevBtn.addEventListener("click", prevSlide);
+nextBtn.addEventListener("click", nextSlide);
+
+// Show the first slide initially
+showSlide(currentIndex);
